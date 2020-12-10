1cc419163659ba9f1c7ff1da489a2202579e0db7
Arich.net <arich.net@gmail.com>
2020-12-10 09:55:57 +0100

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+# Modules used
+# use strict;
+use warnings;
+
+$hello = "Hello World";
+$var2 = "Var1";
+$var3 = "Var3";
+$var4 = "Var4";
+$var5 = "Var5";
+$var6 = "Var6";
+$var7 = "Var7";
+
+# Print function
+print("$hello \n");
